549af7df9c4bd04dd2d766790f0ddb5d6031cfa9
Harrison Stokes <71200667+MachineKiller36@users.noreply.github.com>
2023-09-18 10:49:34 -0700

    Update lab03.cc

--- Lab03/lab03.cc
+++ Lab03/lab03.cc
@@ -1,30 +1,40 @@
 #include <iostream>
+//// Selection Sort Algorithm ////
+/*
+        Parameters:
+                array size and the array itself.
 
+        How it works:
+
+ */
 void selection(int size, int data[]) {
-	for(int index = 1; index < size; index++) {
-		int element = data[index];
-		int position = index-1;
-		while(position > -1 && element < data[position]) {
-			data[position+1] = data[position];
-			position--;
-		}
-		data[position+1] = element;
-	}
+        for(int index = 0; index < size-1; index++) {
+                int smallest = index;
+                for(int position = index+1; position < size; position++) {
+                        if(data[position] < data[smallest]) {
+                                smallest = position;
+                        }
+                }
+                int temp = data[index];
+                data[index] = data[smallest];
+                data[smallest] = temp;
+        }
 }
 
+//// Selection Sort Algorithm ////
+/*
+        Parameters is the array size and the array itself.
+ */
 void bubble(int size, int data[]) {
-	bool notDone = true;
-	while(notDone) {
-		notDone = false;
-		for(int index = 0; index < size; index++) {
-			if(data[index] > data[index+1]) {
-				notDone = true;
-				int temp = data[index+1];
-				data[index+1] = data[index];
-				data[index] = temp;
-			}
-		}
-	}
+        for(int index = 0; index< size; index++) {
+                for(int position = size; position > index; position--) {
+                        if(data[position] < data[position-1]) {
+                                int temp = data[position];
+                                data[position] = data[position-1];
+                                data[position-1] = temp;
+                        }
+                }
+        }
 }
 
 int main() {
