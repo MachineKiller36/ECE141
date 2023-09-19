056c4522145f469e86774a9141b8f5c4b757dac5
Harrison Stokes <71200667+MachineKiller36@users.noreply.github.com>
2023-09-18 20:18:08 -0700

    Update lab03.cc

--- Lab03/lab03.cc
+++ Lab03/lab03.cc
@@ -27,7 +27,7 @@ void selection(int size, long long int data[]) {
                                 smallest = position;                            // Sets smallest equal to position
                         }
                 }
-                long long int temp = data[index];                                               // Temporary variable to hold data at index values
+                long long int temp = data[index];                                // Temporary variable to hold data at index values
                 data[index] = data[smallest];                                   // Sets data at index with the value at data smallest
                 data[smallest] = temp;                                          // Sets data at smallest with temporary variable
         }
@@ -64,58 +64,60 @@ int main() {
         Timer time;
         //// User Interface ///////////
         while(true) {
-                std::cout << "Enter the size of the array you want to test:\n\t! Entering a negative or a zero stops manual input section !\n> ";
+                std::cout << "Enter the size of the array you want to test:\n\t! Entering a negative or a zero stops manual input section !\n> ";         // Prompts User
                 int user_size = 0;
-                std::cin >> user_size;
-                if(user_size < 1) break;
+                std::cin >> user_size;    // Takes in user input                                                                                  
+                if(user_size < 1) break;  // Exits user interface if input is less than 1
 
-                long long int user_array[user_size];
-                long long int user_array_2[user_size];
+                long long int user_array[user_size];      // Creates an array with user defined size
+                long long int user_array_2[user_size];    // Creates an array with user defined size
 
-                std::cout << "Enter a positive value to manual input elements or enter a negative to randomize inputs:\n > ";
+                std::cout << "Enter a positive value to manual input elements or enter a negative to randomize inputs:\n > ";  // Prompts user
                 int user_choice = 0;
-                std::cin >> user_choice;
+                std::cin >> user_choice;     // Takes in user input
 
                 if(user_choice < 0) {
-                        std::srand(std::time(0));
+                        std::srand(std::time(0));  // Prevents duplicates
                         for(int index = 0; index < user_size; index++) {
+                                // Generates random value and stores it in the arrays
                                 long long int value = (std::rand() & 1000) + 1;
                                 user_array[index] = value;
                                 user_array_2[index] = value;
                         }
                 } else {
                         for(int index = 0; index < user_size; index++) {
-                                std::cout << "Enter a value for the array at index " << index << "\n > ";
+                                std::cout << "Enter a value for the array at index " << index << "\n > "; // Prompts the user
                                 long long int user_element = 0;
-                                std::cin >> user_element;
-                                user_array[index] = user_element;
-                                user_array_2[index] = user_element;
+                                std::cin >> user_element;             // Takes in user input
+                                user_array[index] = user_element;     //  Inserts input into the array
+                                user_array_2[index] = user_element;   // Inserts input into the array
                         }
                 }
                 std::cout << "User array:\n";
                 for(int index = 0; index < user_size; index++) {
-                        std::cout << user_array[index] << "\t";
+                        std::cout << user_array[index] << "\t";      // Prints unmodified array
                 }
                 std::cout << "\n\nSelection sort:\n";
-                time.start();
-                selection(user_size,user_array);
-                time.stop();
-                time.print_screen();
+                time.start();                                        // Takes start time
+                selection(user_size,user_array);                     // Array 1 undergoes selection sort
+                time.stop();                                         // Takes end time
+                time.print_screen();                                 // Prints time results
 
                 std::cout << "\nBubble sort:\n";
-                time.start();
-                bubble(user_size, user_array_2);
-                time.stop();
-                time.print_screen();
+                time.start();                                        // Takes start time
+                bubble(user_size, user_array_2);                     // Array 2 undergoes bubble sort
+                time.stop();                                         // Takes end time
+                time.print_screen();                                 // Prints time results
+                
                 std::cout << "Selection sort resulting array:\n";
                 for(int index = 0; index < user_size; index++) {
-                        std::cout << "\t" << user_array[index];
+                        std::cout << "\t" << user_array[index];      // Prints list order thats under went selection sort
                 }
                 std::cout << "\nBubble sort resulting array:\n";
                 for(int index = 0; index < user_size; index++) {
-                        std::cout << "\t" << user_array_2[index];
+                        std::cout << "\t" << user_array_2[index];   // Prints list order thats under went bubble sort
                 }
-                std::cout << "\n\n\n\n\n\n\n";
+                std::cout << "\n\n\n\n\n\n\n";                      // "Clears" terminal screen
         }
 
         //// Test Bench ////
